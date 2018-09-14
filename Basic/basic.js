7a1f0c16f09136c1afc8a3f2be4b87c0b5746a4b
EmirDelicR <emirdelictbf@gmail.com>
2018-09-14 15:32:24 +0200

    More examples in basic.js file

--- Basic/basic.js
+++ Basic/basic.js
@@ -1,11 +1,11 @@
 /*----------------------- Logical operators and short-circuit evaluation -----------------------------*/
 console.log('-----------------Logical operators and short-circuit evaluation-----------------');
 var test = true;
-var isTrue = function() {
+let isTrue = function() {
     console.log('This is true');
 }
 
-var isFalse = function() {
+let isFalse = function() {
     console.log('This is false');
 }
 
@@ -15,57 +15,57 @@ var isFalse = function() {
 
 // In functions to set default var
 function test(attr) {
-    var atribute = attr || 'default';
+    let atribute = attr || 'default';
 }
 
 /*---------------------------------------- Common Array Methods --------------------------------------*/
 console.log('--------------Common Array Methods-----------------');
-var arr = ['a', 'b', 'c'];
+let arr = ['a', 'b', 'c'];
 console.log('Normal array', arr);
 arr.push("d"); // Add element at end
 console.log('Array call push method', arr);
-var pop = arr.pop() // remove and return last element
+let pop = arr.pop() // remove and return last element
 console.log('Result of arr.pop() store in pop variable: ', pop);
 console.log('Array call pop method', arr);
-var arr2 = ['e', 'f'];
-var arr_concat = arr.concat(arr2); // conbine two arrays
+let arr2 = ['e', 'f'];
+let arr_concat = arr.concat(arr2); // conbine two arrays
 console.log('Array call concat method', arr);
 console.log('arr2 after calling concat: ', arr2);
 console.log('arr_concat that store result of conct method on arr and arr2', arr_concat);
-var arr_join = arr.join("--"); // join elements 
+let arr_join = arr.join("--"); // join elements 
 console.log('Array call join method', arr);
 console.log('arr_join after calling join: ', arr_join);
 arr.reverse(); // reverse array
 console.log('Array call reverse method', arr);
 arr.reverse();
-var shift = arr.shift(); // Remove first element
+let shift = arr.shift(); // Remove first element
 console.log('Array call shift method', arr);
 console.log('Result of arr.shift() store in shift variable: ', shift);
-var unshift = arr.unshift('k'); // Add element on first place
+let unshift = arr.unshift('k'); // Add element on first place
 console.log('Array call unshift method', arr);
 console.log('Result of arr.unshift() store in unshift variable: ', unshift);
-var slice = arr.slice(1,3); // slice array from index to index
+let slice = arr.slice(1,3); // slice array from index to index
 console.log('Array call slice method', arr);
 console.log('Result of arr.slice() store in slice variable: ', slice);
 arr.sort(); // sort array
 console.log('Array call sort method', arr);
-var splice = arr.splice(1,2, "Test"); // Splice array and add new entry
+let splice = arr.splice(1,2, "Test"); // Splice array and add new entry
 console.log('Array call splice method', arr);
 console.log('Result of arr.splice() store in splice variable: ', splice);
 
 /*---------------------------------------- Copying Arrays (deep and shallow) --------------------------------------*/
 console.log('-----------------Copying Arrays (deep and shallow)---------------');
-var newArray = [1, 2, 3, 4, 5, 6];
+let newArray = [1, 2, 3, 4, 5, 6];
 // slice
 console.log('Original array: ', newArray);
-var copy_slice = newArray.slice(0); // copy array using slice
+let copy_slice = newArray.slice(0); // copy array using slice
 console.log('Array copyed using slice: ', copy_slice);
 // spred operator
-var copy_spred = [...newArray];
+let copy_spred = [...newArray];
 console.log('Array copyed using spred operator ... : ', copy_spred);
 // Array.from() method
-var orginal_arr = [[1,2], [3,4], 5];
-var arr_copy = Array.from(orginal_arr);
+let orginal_arr = [[1,2], [3,4], 5];
+let arr_copy = Array.from(orginal_arr);
 console.log('Original array: ', orginal_arr);
 console.log('Copy of orginal array: ', arr_copy);
 console.log('Now change arr_copy array arr_copy[0].push(3): ');
@@ -75,7 +75,7 @@ console.log('Copy of orginal array: ', arr_copy);
 
 // Deep copy
 function copy(o) {
-    var output, v, key;
+    let output, v, key;
     output = Array.isArray(o) ? [] : {};
     for (key in o) {
         v = o[key];
@@ -84,8 +84,8 @@ function copy(o) {
     return output;
  }
 
-var deep_arr = [[1,2], [3,4], 5];
-var deep_copy = copy(deep_arr);
+let deep_arr = [[1,2], [3,4], 5];
+let deep_copy = copy(deep_arr);
 console.log('Original array: ', deep_arr);
 console.log('Copy of deep array: ', deep_copy);
 console.log('Now change deep_copy array deep_copy[0].push(3): ');
@@ -94,9 +94,54 @@ console.log('Deep array: ', deep_arr);
 console.log('Copy of deep array: ', deep_copy);
 
 // deep copy using JSON it is not recomendet for large arrays or objects
-var a = [[1,2], [3,4], 5];
-var b = JSON.parse(JSON.stringify(a));
+let a = [[1,2], [3,4], 5];
+let b = JSON.parse(JSON.stringify(a));
 console.log(b); // 
 b[0].push(17);
 console.log('Deep array using JSON: ', a);
 console.log('Copy of deep array using JSON: ', b);
+
+/**------------------------- Random numbers & parseInt ------------------------------- */
+console.log('-------------------Random numbers & parseInt-----------------------');
+let rand_num = Math.round(Math.random() * 10); // from 0-10
+console.log('Random number : ', rand_num);
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max-min +1)) + min;
+}
+console.log('Random number in the range 3-9: ', randomRange(3, 9));
+
+console.log('Parsing string "007" to integer: ',parseInt("007"));
+
+/**--------------------------------- for in / for of loops  ------------------------------ */
+console.log('----------------- for in / for of loops ------------------------------');
+
+let person = {fname: 'Emir', lname: 'Delic', age: 31};
+console.log('for...in loop only iterates over enumerable');
+for(let key in person) {
+    console.log('Example for for in loop: ', key+' : '+person[key]);
+}
+
+let iterable = [10, 20, 30];
+
+for (const value of iterable) {
+  console.log('Example for for of loop: ', value);
+}
+
+/**--------------------------------- Array Iteration: 8 Methods ---------------------------- */
+console.log('Array Iteration: 8 Methods');
+
+let arr_methods = [1, 2, 3, 4, 5];
+// forEach
+
+// map
+
+// filter
+
+// reduce
+
+// some
+
+// every
+
+// find
