cc11ec20491b85a048d5db44cbcfe85120936e8e
Emir <emirdelictbf@gmail.com>
2019-08-18 13:50:13 +0200

    Claning up

--- FunctionalPrograming/highOrderFun.js
+++ FunctionalPrograming/highOrderFun.js
@@ -1,16 +1,16 @@
 /** ---------------------------- Filter --------------------------------- */
 
 let animals = [
-    {name: 'A' , species: 'dog'},
-    {name: 'B' , species: 'cat'},
-    {name: 'C' , species: 'fish'},
-    {name: 'D' , species: 'dog'},
-    {name: 'E' , species: 'cat'},
-    {name: 'F' , species: 'dog'},
-    {name: 'G' , species: 'dog'},
+  { name: "A", species: "dog" },
+  { name: "B", species: "cat" },
+  { name: "C", species: "fish" },
+  { name: "D", species: "dog" },
+  { name: "E", species: "cat" },
+  { name: "F", species: "dog" },
+  { name: "G", species: "dog" }
 ];
 
-/** manualy exampl: */
+/** manually example: */
 /*
 let dogs = [];
 for (let i= 0; i<animals.length; i++){
@@ -20,13 +20,13 @@ for (let i= 0; i<animals.length; i++){
 }
 */
 // using filter function
-let dogs = animals.filter(animal => animal.species === 'dog');
-let rest = animals.filter(animal => !(animal.species === 'dog'));
+let dogs = animals.filter(animal => animal.species === "dog");
+let rest = animals.filter(animal => !(animal.species === "dog"));
 console.log(dogs);
 console.log(rest);
 
 /** ------------------------------------ Map ---------------------------------- */
-/** manualy exampl: */
+/** manually example: */
 /*
 let names = [];
 for (let i= 0; i<animals.length; i++){
@@ -35,20 +35,19 @@ for (let i= 0; i<animals.length; i++){
 */
 // Using map
 let names = animals.map(animal => `${animal.name} is a ${animal.species}`);
-console.log(names); 
-
+console.log(names);
 
 /** ------------------------------ Reduce ---------------------------------------- */
 let orders = [
-    {amount: 150},
-    {amount: 250},
-    {amount: 400},
-    {amount: 100},
+  { amount: 150 },
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 }
 ];
 
 let ordersFlat = [150, 250, 400, 100];
 
-/** Manualy */
+/** manually example */
 /*
 let total = 0;
 for (let i= 0; i<orders.length; i++){
@@ -56,10 +55,10 @@ for (let i= 0; i<orders.length; i++){
 }
 */
 
-let total = orders.reduce( (total, order) => {
-    console.log(`Sum: ${total} and order: ${order.amount}`); 
-    return total + order.amount;
-} , 0);
+let total = orders.reduce((total, order) => {
+  console.log(`Sum: ${total} and order: ${order.amount}`);
+  return total + order.amount;
+}, 0);
 /**
  * Short
  * let total = orders.reduce( (total, order) => total + order.amount , 0);
@@ -67,8 +66,7 @@ let total = orders.reduce( (total, order) => {
 console.log(total);
 
 // Turn this in json
-let string = 
-`emir delic  waffle iron  80  2
+let string = `emir delic  waffle iron  80  2
 emir delic  blender  200  1
 emir delic  knife  10  4
 amir delic  waffle iron  80  1
@@ -76,51 +74,50 @@ amir delic  knife  10  2
 amir delic  pot  20  3`;
 
 const convertString = () => {
-    let tab = '  ';
-    let tmpArray = string.split('\n')
-        .map(line => line.split(tab))
-        .reduce( (customers, line) => {
-            customers[line[0]] = customers[line[0]] || [];
-            let obj = {
-                name: line[1],
-                price: line[2],
-                quant: line[3]
-            };
-            customers[line[0]].push(obj);
-            
-            return customers;
-        }, {}); 
-    
-    return tmpArray;
+  let tab = "  ";
+  let tmpArray = string
+    .split("\n")
+    .map(line => line.split(tab))
+    .reduce((customers, line) => {
+      customers[line[0]] = customers[line[0]] || [];
+      let obj = {
+        name: line[1],
+        price: line[2],
+        quant: line[3]
+      };
+      customers[line[0]].push(obj);
 
+      return customers;
+    }, {});
+
+  return tmpArray;
 };
 
 console.log(convertString());
 
 /** ----------------------------   Closures -------------------------------------------- */
-// A closure is the combination of a function and the lexical environment within which that function was declared. 
+// A closure is the combination of a function and the lexical environment within which that function was declared.
 
 function makeFunc() {
-    var name = `Closures all: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures`;
-    function displayName() {
-      console.log(name);
-    }
-    return displayName;
+  var name = `Closures all: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures`;
+  function displayName() {
+    console.log(name);
   }
-  
-  var myFunc = makeFunc();
-  myFunc();
+  return displayName;
+}
 
+var myFunc = makeFunc();
+myFunc();
 
 /** ----------------------------   Currying -------------------------------------------- */
-// Creates a function that accepts arguments of func and either invokes func returning its result, 
-// if at least arity number of arguments have been provided, or returns a function that accepts the remaining func arguments, and so on. 
+// Creates a function that accepts arguments of func and either invokes func returning its result,
+// if at least arity number of arguments have been provided, or returns a function that accepts the remaining func arguments, and so on.
 // The arity of func may be specified if func.length is not sufficient.
 let dragon = (a, b, c) => `${a} is a ${b} dragon that breaths ${c}!`;
-console.log(dragon('Bog', 'tiny', 'fire'));
+console.log(dragon("Bog", "tiny", "fire"));
 // now in currying
 let dragon2 = a => b => c => `${a} is a ${b} dragon that breaths ${c}!`;
-console.log(dragon2('Bog')('tiny')('fire'));
-console.log("Lo!!!", 'https://lodash.com/docs#curry');
+console.log(dragon2("Bog")("tiny")("fire"));
+console.log("Lo!!!", "https://lodash.com/docs#curry");
 
 /** ----------------------------   Closures -------------------------------------------- */
